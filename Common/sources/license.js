0b9159569a28736b59ae849fd4f3d8bb81a6d944
Forevka <32968153+Forevka@users.noreply.github.com>
2025-12-07 17:49:35 +0200

    Update license settings for Developer mode

--- Common/sources/license.js
+++ Common/sources/license.js
@@ -45,17 +45,17 @@ exports.readLicense = async function () {
     {
       count: 1,
       type: c_LR.Success,
-      packageType: constants.PACKAGE_TYPE_OS,
-      mode: constants.LICENSE_MODE.None,
-      branding: false,
+      packageType: constants.PACKAGE_TYPE_D,
+      mode: constants.LICENSE_MODE.Developer,
+      branding: true,
       connections: constants.LICENSE_CONNECTIONS,
       connectionsView: constants.LICENSE_CONNECTIONS,
-      customization: false,
-      advancedApi: false,
+      customization: true,
+      advancedApi: true,
       usersCount: 0,
       usersViewCount: 0,
       usersExpire: constants.LICENSE_EXPIRE_USERS_ONE_DAY,
-      hasLicense: false,
+      hasLicense: true,
       buildDate: oBuildDate,
       startDate,
       endDate: null,
@@ -67,4 +67,4 @@ exports.readLicense = async function () {
   ];
 };
 
-exports.packageType = constants.PACKAGE_TYPE_OS;
+exports.packageType = constants.PACKAGE_TYPE_D;
